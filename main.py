3f687d21b6411e6a0871c60833c6c11e9509dd05
vagrant <vagrant@homestead>
2019-03-17 07:08:41 +0000

    fixed errors

--- main.py
+++ main.py
@@ -9,17 +9,23 @@ def my_form():
 def my_form_tech_skills():
     tech_skills = request.form['text']
     return tech_skills
+@app.route('/', methods=['soft_skills'])
 
 def my_form_soft_skills():
     tech_skills = request.form['text']
+
     return soft_skills
+@app.route('/', methods=['gpa'])
 
 def my_form_gpa():
     gpa = request.form['text']
     return gpa
+@app.route('/', methods=['majors'])
+
 def my_form_majors():
     majors = request.form['text']
     return tech_skills
+@app.route('/', methods=['expected_graduation'])
 
 def my_form_expected_graduation():
     expected_graduation = request.form['text']
